d51258413101ba008ec04ad15c5a5d7e5fa6f4e0
JAFAJ0 <86894074+JAFAJ0@users.noreply.github.com>
2021-07-04 11:54:17 -0400

    Create isDigit.java

---
+++ isDigit.java
@@ -0,0 +1,35 @@
+/*
+Determine if the given character is a digit or not.
+
+Example
+
+For symbol = '0', the output should be
+isDigit(symbol) = true;
+For symbol = '-', the output should be
+isDigit(symbol) = false.
+Input/Output
+
+[execution time limit] 3 seconds (java)
+
+[input] char symbol
+
+A character which is either a digit or not.
+
+Guaranteed constraints:
+Given symbol is from ASCII table.
+
+[output] boolean
+
+true if symbol is a digit, false otherwise.
+*/
+boolean isDigit(char symbol) {
+    try{
+        Integer.parseInt(String.valueOf(symbol));
+    }
+    catch(Exception e){
+        return false;
+    }
+    return true;
+
+}
+//So easy
